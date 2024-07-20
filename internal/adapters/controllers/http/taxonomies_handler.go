0f74d69547b55ed8640d2ebd2a3268f5b4f61bd6
Yeganeh <yeganeh.rad434@gmail.com>
2024-07-20 21:23:46 +0330

    implement tag relationship setup

--- internal/adapters/controllers/http/taxonomies_handler.go
+++ internal/adapters/controllers/http/taxonomies_handler.go
@@ -3,14 +3,20 @@ package http
 import (
 	"log"
 	"tag_project/internal/adapters/databases/mysql"
-	"tag_project/internal/domain/entity"
 	"tag_project/internal/application/usecases"
+	"tag_project/internal/domain/entity"
+
 	"github.com/gin-gonic/gin"
 )
 
 type Taxonomy struct {
 	FromTag          uint64 `json:"fromTag"`
 	ToTag            uint64 `json:"toTag"`
+	RelationshipType string `json:"relationshipType"`
+}
+
+type TagRelationship struct {
+	ID               uint64 `json:"id"`
 	RelationshipType string `json:"relationshipType"`
 }
 
@@ -23,31 +29,46 @@ func RegisterTagRelationship(c *gin.Context) {
 		return
 	}
 	taxonomyID, err := usecases.GenerateID()
-	if err!=nil {
-		log.Printf("error generating ID:%v",err)
+	if err != nil {
+		log.Printf("error generating ID:%v", err)
 		c.Status(400)
 		return
 	}
-	taxonomyInfo:=entity.Taxonomy{
-		ID:taxonomyID,
-		FromTag: requestBody.FromTag,
-		ToTag: requestBody.ToTag,
+	taxonomyInfo := entity.Taxonomy{
+		ID:               taxonomyID,
+		FromTag:          requestBody.FromTag,
+		ToTag:            requestBody.ToTag,
 		RelationshipType: requestBody.RelationshipType,
-		Status:"active",
+		Status:           "active",
 	}
-	err=mysql.TagDB.RegisterTagRelationship(taxonomyInfo)
-	if err!=nil{
-		log.Printf("error registering tag relationship:%v",err)
+	err = mysql.TagDB.RegisterTagRelationship(taxonomyInfo)
+	if err != nil {
+		log.Printf("error registering tag relationship:%v", err)
 		c.Status(400)
 		return
 	}
-	c.JSON(200,gin.H{
-		"message":"tags relationship registered seccussfully",
+	c.JSON(200, gin.H{
+		"message": "tags relationship registered seccussfully",
 	})
 }
 
 func SetTagRelationship(c *gin.Context) {
-
+	var requestBody TagRelationship
+	err:=c.BindJSON(&requestBody)
+	if err!=nil {
+		log.Printf("error binding JSON:%v",err)
+		c.Status(400)
+		return
+	}
+	err=mysql.TagDB.SaveTagRelationship(requestBody.ID,requestBody.RelationshipType)
+	if err!=nil {
+		log.Printf("error saving tag relationship:%v",err)
+		c.Status(400)
+		return
+	}
+	c.JSON(200, gin.H{
+		"message": "tag relationship setup was successful",
+	})
 }
 
 func GetRelatedTagsByKey(c *gin.Context) {
--- internal/adapters/databases/mysql/taxonomies_repository.go
+++ internal/adapters/databases/mysql/taxonomies_repository.go
@@ -2,9 +2,17 @@ package mysql
 
 import "tag_project/internal/domain/entity"
 
-func (d *Database) RegisterTagRelationship(taxonomyInfo entity.Taxonomy) error{
-	result:=d.db.Create(&taxonomyInfo)
-	if result.Error!=nil{
+func (d *Database) RegisterTagRelationship(taxonomyInfo entity.Taxonomy) error {
+	result := d.db.Create(&taxonomyInfo)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (d *Database) SaveTagRelationship(ID uint64, relationshipType string) error {
+	result:=d.db.Model(&entity.Taxonomy{}).Where("id=?",ID).Update("RelationshipType",relationshipType)
+	if result.Error!=nil {
 		return result.Error
 	}
 	return nil
